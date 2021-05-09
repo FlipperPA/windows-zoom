cab1abc8dc1e642279b87856c845ad3bce5bacee
Timothy Allen <flipper@peregrinesalon.com>
2021-05-09 07:01:45 -0700

    Optional Zoom Meeting

--- windows-setup.ps1
+++ windows-setup.ps1
@@ -48,14 +48,16 @@ rm ZoomInstallerFull.msi
 
 Write-Output("We'll create a shortcut on the Desktop to start your meeting! If you don't want this, just don't enter anything.")
 $Meeting_ID = Read-Host "Enter your Zoom Meeting ID Number (example: 123456789)"
-$Meeting_PW = Read-Host "Enter your Zoom Meeting Hashed Password (example: U0MeOUpxS1BpRmc2ExzU1WjZErUUQT09)"
+if($Meeting_ID.Length) {
+    $Meeting_PW = Read-Host "Enter your Zoom Meeting Hashed Password (example: U0MeOUpxS1BpRmc2ExzU1WjZErUUQT09)"
 
-$WshShell = New-Object -comObject WScript.Shell
-$Shortcut = $WshShell.CreateShortcut("$Home\Desktop\Launch Zoom Meeting.lnk")
-$Shortcut.TargetPath = "C:\Program Files (x86)\Zoom\bin\Zoom.exe"
-$Shortcut.Arguments = '"--url=zoommtg://zoom.us/join?confno=' + $Meeting_ID + '&pwd=' + $Meeting_PW + '&zc=0&uname=AA Meeting"'
-$Shortcut.WorkingDirectory = "C:\Program Files (x86)\Zoom\bin"
-$Shortcut.Save()
+    $WshShell = New-Object -comObject WScript.Shell
+    $Shortcut = $WshShell.CreateShortcut("$Home\Desktop\Launch Zoom Meeting.lnk")
+    $Shortcut.TargetPath = "C:\Program Files (x86)\Zoom\bin\Zoom.exe"
+    $Shortcut.Arguments = '"--url=zoommtg://zoom.us/join?confno=' + $Meeting_ID + '&pwd=' + $Meeting_PW + '&zc=0&uname=AA Meeting"'
+    $Shortcut.WorkingDirectory = "C:\Program Files (x86)\Zoom\bin"
+    $Shortcut.Save()
+}
 
 $WshShell = New-Object -comObject WScript.Shell
 $Shortcut = $WshShell.CreateShortcut("$Home\Desktop\Shutdown Computer.lnk")
