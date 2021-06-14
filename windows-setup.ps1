8e0b2979cefa53c19408812a922042ed96b5e9ae
Timothy Allen <flipper@peregrinesalon.com>
2021-06-14 16:09:06 -0700

    Update windows-setup.ps1

--- windows-setup.ps1
+++ windows-setup.ps1
@@ -69,7 +69,7 @@ while (1) {
     # Automatically launch Zoom into the first meeting added.
     if($Meeting_Count -eq 1) {
         $StartUp = "$Env:USERPROFILE\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
-        New-Item -ItemType SymbolicLink -Path "$StartUp" -Name "ZoomAutoStart.lnk" -Value $Shortcut_Path
+        New-Item -ItemType SymbolicLink -Path "$StartUp" -Name "ZoomAutoStart.lnk" -Value "$Shortcut_Path"
     }
 }
 
