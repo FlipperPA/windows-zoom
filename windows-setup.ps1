f10cc2463d90a37536538667a2b4bc954170eeec
Timothy Allen <flipper@peregrinesalon.com>
2021-06-14 16:05:31 -0700

    GAH POWERSHELL

--- windows-setup.ps1
+++ windows-setup.ps1
@@ -66,7 +66,7 @@ while (1) {
     $Shortcut.Save()
     
     # Automatically launch Zoom into the first meeting added.
-    if($Meeting_Count == 1) {
+    if($Meeting_Count -eq 1) {
         $StartUp = "$Env:USERPROFILE\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
         New-Item -ItemType SymbolicLink -Path "$StartUp" -Name "ZoomAutoStart.lnk" -Value "$Home\Desktop\Launch Zoom " + $Meeting_Name + ".lnk"
     }
